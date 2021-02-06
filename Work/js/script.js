3817e7b0cdd31a17a446b3775466ec158f1f2df6
Rodion <rodion.gatskevich@mail.ru>
2021-02-07 02:07:51 +0300

    first

---
+++ Work/js/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+// alert(5);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj["name"]);
+
+// let arr = ['plum', 'orange.jpg', 'apple.bmp', 4];
+// console.log(arr[1]);
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const aswer = prompt('Вам есть 18?');
+// console.log(aswer);
+
+// const answer = [];
+
+// answer[0] = prompt('Как ваше имя?', '');
+// answer[1] = prompt('Как ваше Фамилия?', '');
+// answer[2] = prompt('Как ваше Отчество?', '');
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+console.log('arr' + "- object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4==='8');
+
